c3275d620a163b148e7b33bea89d0997529819a8
Babin-exe <155212596+Babin-exe@users.noreply.github.com>
2024-07-26 19:29:43 +0545

    Update 20_problem.c

--- 20_problem.c
+++ 20_problem.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
-int main()
-{
-    int n, f = 1;
 
-    printf("Enter a number : ");
+int main() {
+    int n, first = 0, second = 1, next;
+
+    printf("Enter the number of terms: ");
     scanf("%d", &n);
 
-    if (n < 0)
-    {
-        printf("Factorial doesnt exists\n");
+    if (n < 2) {
+        printf("Number of terms should be at least 2.\n");
+        return 1;
     }
-    else
-    {
 
-        for (int i = 1; i <= n; i++)
-        { 
-            f = f * i;
-        }
-        printf("The factorial of % d is : %d\n", n, f);
+    printf("Fibonacci Series: %d, %d", first, second);
+
+    for (int i = 3; i <= n; ++i) {
+        next = first + second;
+        printf(", %d", next);
+        first = second;
+        second = next;
     }
+    printf("\n");
+
     return 0;
 }
